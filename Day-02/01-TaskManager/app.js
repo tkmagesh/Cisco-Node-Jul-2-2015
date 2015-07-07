79f823d1d312fa192792ad5b48e2aa931cac3e88
tkmagesh <tkmagesh77@gmail.com>
2015-07-07 12:38:17 +0530

    Task Toggle

--- Day-02/01-TaskManager/app.js
+++ Day-02/01-TaskManager/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var qs = require("querystring");
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var taskRoutes = require('./routes/taskRoutes.js');
@@ -18,6 +20,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- Day-02/01-TaskManager/routes/taskRoutes.js
+++ Day-02/01-TaskManager/routes/taskRoutes.js
@@ -3,13 +3,45 @@ var router = express.Router();
 
 var taskList = [
     {id :1, name : "Book tickes for the movie", isCompleted : false},
-    {id :2, name : "Plan for the travel", isCompleted : false},
+    {id :2, name : "Plan for the travel", isCompleted : true},
     {id :3, name : "Fix the bug", isCompleted : false}
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(taskList);
+    var viewData = {list : taskList};
+    res.render('tasks/index', viewData);
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('tasks/new');
+});
+
+router.post('/new', function(req, res, next) {
+
+  var newId = taskList.reduce(function(result, task){
+      return result > task.id ? result : task.id;
+  }, 0) + 1;
+
+  var newTask = {
+      id : newId,
+      name : req.body.taskname,
+      isCompleted : false
+  };
+  taskList.push(newTask);
+  res.redirect('/tasks');
+});
+
+router.get("/toggle/:id", function(req, res, next){
+   var taskId = parseInt(req.param("id"),10);
+    var task = taskList.filter(function(task){
+        return task.id === taskId;
+    })[0];
+    if (task){
+        task.isCompleted = !task.isCompleted;
+    }
+    res.redirect('/tasks');
+});
+
 
 module.exports = router;
